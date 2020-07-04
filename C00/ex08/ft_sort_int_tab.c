0def66f07a25f72ab3c3aa13477dba579247e903
Sunghyun Gang <sgang@c3r1s2.42seoul.kr>
2020-07-04 23:52:31 +0900

    C00

--- C00/ex08/ft_sort_int_tab.c
+++ C00/ex08/ft_sort_int_tab.c
@@ -1,1 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sgang </var/mail/sgang>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/04 23:18:24 by sgang             #+#    #+#             */
+/*   Updated: 2020/07/04 23:51:42 by sgang            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void ft_sort_int_tab(int *tab, int size)
+{
+	int temp;
+	
+	int i;
+	int j;
+	int least;
+	i = 0;
+	while(i < size)
+	{
+		least = j = i;
+		while(++j < size)
+			if(tab[j] < tab[least])
+				least = j;
+		temp = arr[least];
+		arr[least] = arr[i];
+		arr[i++] = temp;
+	}
+}
 
