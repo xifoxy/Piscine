20f81ac62e0bcba148ebc85bd8c120e769c30dfc
Sunghyun Gang <sgang@c3r1s2.42seoul.kr>
2020-07-08 19:27:18 +0900

    C02

--- C02/ex01/ft_strncpy.c
+++ C02/ex01/ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: sgang <xifoxy.ru@gmail.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/06 17:48:37 by sgang             #+#    #+#             */
-/*   Updated: 2020/07/06 20:02:18 by sgang            ###   ########.fr       */
+/*   Updated: 2020/07/08 17:51:16 by sgang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 	int idx;
 
 	while (idx < n && src[idx])
-		dest[idx] = src[idx];
+		dest[idx] = src[idx++];
+	while(idx < n)
+		dest[idx++] = 0;
 	return (dest);
 }
--- C02/ex09/ft_strcapitalize.c
+++ C02/ex09/ft_strcapitalize.c
@@ -6,28 +6,44 @@
 /*   By: sgang <xifoxy.ru@gmail.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/06 18:35:21 by sgang             #+#    #+#             */
-/*   Updated: 2020/07/06 18:44:21 by sgang            ###   ########.fr       */
+/*   Updated: 2020/07/08 19:26:43 by sgang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		islower(char ch)
+int		is_num(char ch)
+{
+	return (ch >= '0' && ch <= '9');
+}
+
+int		is_lower(char ch)
 {
 	return (ch >= 'a' && ch <= 'z');
 }
 
-int		isfirst(char ch)
+int		is_upper(char ch)
 {
-	return (islower(ch) || (ch >= 'A' && ch <= 'Z'));
+	return (ch >= 'A' && ch <= 'Z');
+}
+
+int		is_word(char ch)
+{
+	return (is_num(ch) || is_lowr(ch) || is_upper(ch));
 }
 
 char	*ft_strcapitalize(char *str)
 {
 	int idx;
 
-	idx = 0;
+	idx = 1;
+	if (is_lower(str[0]))
+		str[idx] += -32;
 	while (str[idx])
 	{
-		if (islower(str[idx]) && (idx == 0 || !isfirst(str[idx - 1])))
+		if (is_upper(str[idx]))
+			str[idx] -= -32;
+		if (is_word(str[idx - 1]))
+			;
+		else if (is_lower(str[idx]))
 			str[idx] += -32;
 		++idx;
 	}
