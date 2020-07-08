e456ad6df8fd0e79ea6487a6473f50dfdfdba8a5
Sunghyun Gang <sgang@c3r1s2.42seoul.kr>
2020-07-08 21:36:19 +0900

    C02

--- C02/ex11/ft_putstr_non_printable.c
+++ C02/ex11/ft_putstr_non_printable.c
@@ -6,7 +6,7 @@
 /*   By: sgang <xifoxy.ru@gmail.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/06 20:30:48 by sgang             #+#    #+#             */
-/*   Updated: 2020/07/08 21:28:50 by sgang            ###   ########.fr       */
+/*   Updated: 2020/07/08 21:36:08 by sgang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int		isprint(unsigned char ch)
 void	hprint(unsigned char ch)
 {
 	ch += '0';
-	ch = ch > '9' ? 39 : 0;
+	ch += ch > '9' ? 39 : 0;
 	write(1, &ch, 1);
 }
 
@@ -33,7 +33,7 @@ void	ft_putstr_non_printable(char *str)
 	{
 		if (!isprint(str[idx]))
 		{
-			write(1, '\\', 1);
+			write(1, "\\", 1);
 			hprint(str[idx] / 16);
 			hprint(str[idx] % 16);
 		}
