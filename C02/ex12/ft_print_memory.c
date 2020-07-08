127a918914ad7e01aba9c7c4fc0475a40c5a84c8
Sunghyun Gang <sgang@c3r1s2.42seoul.kr>
2020-07-08 23:01:05 +0900

    C02

--- C02/ex12/ft_print_memory.c
+++ C02/ex12/ft_print_memory.c
@@ -3,14 +3,87 @@
 /*                                                        :::      ::::::::   */
 /*   ft_print_memory.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sgang </var/mail/sgang>                    +#+  +:+       +#+        */
+/*   By: sgang <xifoxy.ru@gmail.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/07/06 21:19:08 by sgang             #+#    #+#             */
-/*   Updated: 2020/07/06 21:19:56 by sgang            ###   ########.fr       */
+/*   Created: 2020/07/08 21:37:19 by sgang             #+#    #+#             */
+/*   Updated: 2020/07/08 23:00:53 by sgang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void 	*ft_print_memory(void *addr, unsigned int size)
-{
+#include <unistd.h>
 
+char g_str[16];
+
+void	ft_print(char ch)
+{
+	write(1, &ch, 1);
+}
+
+void	ft_translation_hex(char ch)
+{
+	ch += '0';
+	ch += ch > '9' ? 39 : 0;
+	ft_print(ch);
+}
+
+void	ft_make_addr(long long ptr, int cnt)
+{
+	char ch;
+
+	if (!ptr)
+	{
+		while (cnt < 16)
+		{
+			++cnt;
+			write(1, "0", 1);
+		}
+		return ;
+	}
+	ft_make_addr(ptr / 16, cnt + 1);
+	ft_translation_hex(ptr % 16);
+}
+
+void	ft_translation_addr(unsigned char *ptr, unsigned int len)
+{
+	unsigned int idx;
+
+	idx = 0;
+	while (idx < len)
+	{
+		if (!(idx % 2))
+			ft_print(' ');
+		if (ptr[idx] >= ' ' && ptr[idx] <= '~')
+			g_str[idx] = ptr[idx];
+		else
+			g_str[idx] = '.';
+		ft_translation_hex(ptr[idx] / 16);
+		ft_translation_hex(ptr[idx] % 16);
+		++idx;
+	}
+}
+
+void	*ft_print_memory(void *addr, unsigned int size)
+{
+	unsigned int idx;
+	unsigned int len;
+	unsigned char *ptr;
+
+	idx = 0;
+	while (size)
+	{
+		if (size / 16)
+			len = 16;
+		else
+			len = size;
+		ptr = (unsigned char*)arr + idx;
+		ft_make_addr((long long)ptr, 0);
+		ft_print(':');
+		ft_translation_addr(ptr, len);
+		ft_print(' ');
+		write(1, g_str, len);
+		ft_print('\n');
+		size += -len;
+		i -= -16;
+	}
+	return (addr);
 }
