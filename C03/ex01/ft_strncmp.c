a9fd90a98967d2102da89ab19d54f30cc76bbf83
Sunghyun Gang <sgang@c4r3s4.42seoul.kr>
2020-07-14 22:13:26 +0900

    C03

--- C03/ex01/ft_strncmp.c
+++ C03/ex01/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: sgang <xifoxy.ru@gmail.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/08 23:09:22 by sgang             #+#    #+#             */
-/*   Updated: 2020/07/08 23:12:42 by sgang            ###   ########.fr       */
+/*   Updated: 2020/07/14 21:54:32 by sgang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,13 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 	unsigned int idx;
 
 	idx = 0;
-	while (idx < n)
+	while (s1[idx] && s2[idx] && idx < n)
 	{
 		if (s1[idx] != s2[idx])
 			return (s1[idx] - s2[idx]);
 		++idx;
 	}
-	return (0);
+	if (idx == n)
+		--idx;
+	return (s1[idx] - s2[idx]);
 }
