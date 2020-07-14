f5308771633976e439022ffecd6ff3b8cc822111
Sunghyun Gang <sgang@c3r1s2.42seoul.kr>
2020-07-15 00:33:59 +0900

    C02

--- C02/ex01/ft_strncpy.c
+++ C02/ex01/ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: sgang <xifoxy.ru@gmail.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/06 17:48:37 by sgang             #+#    #+#             */
-/*   Updated: 2020/07/14 20:38:33 by sgang            ###   ########.fr       */
+/*   Updated: 2020/07/15 00:33:39 by sgang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,15 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 	unsigned int idx;
 
 	idx = 0;
-	while (idx < n && src[idx]){
+	while (idx < n && src[idx])
+	{
 		dest[idx] = src[idx];
 		++idx;
 	}
-	while (idx < n){
+	while (idx < n)
+	{
 		dest[idx] = 0;
 		++idx;
 	}
 	return (dest);
-}
-#include <stdio.h>
-#include <string.h>
-int main(){
-	char dest[] = "12345";
-	char src[] = "HI";
-
-	strncpy(dest, src, 4);
-
-	
 }
