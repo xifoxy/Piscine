1432077cb62a4d1c5c2d243f83eab30c376dceaf
Sunghyun Gang <sgang@c4r3s4.42seoul.kr>
2020-07-14 22:13:17 +0900

    C00

--- C00/ex07/ft_putnbr.c
+++ C00/ex07/ft_putnbr.c
@@ -6,11 +6,16 @@
 /*   By: sgang <xifoxy.ru@gmail.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/06 15:42:41 by sgang             #+#    #+#             */
-/*   Updated: 2020/07/08 17:16:55 by sgang            ###   ########.fr       */
+/*   Updated: 2020/07/14 22:11:12 by sgang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+
+void	ft_putchar(char ch)
+{
+	write(1, &ch, 1);
+}
 
 void	recur(int num)
 {
@@ -20,7 +25,7 @@ void	recur(int num)
 		return ;
 	ch = (num < 0 ? -(num % 10) : (num % 10)) + '0';
 	recur(num / 10);
-	write(1, &ch, 1);
+	ft_putchar(ch);
 }
 
 void	ft_putnbr(int nb)
