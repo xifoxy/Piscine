9a605ff50ba5df1115b65b5350f4871f8f58a344
Sunghyun Gang <sgang@c4r3s4.42seoul.kr>
2020-07-14 22:13:41 +0900

    C05

--- C05/ex00/ft_iterative_factorial.c
+++ C05/ex00/ft_iterative_factorial.c
@@ -6,7 +6,7 @@
 /*   By: sgang <xifoxy.ru@gmail.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/13 22:25:15 by sgang             #+#    #+#             */
-/*   Updated: 2020/07/13 22:29:19 by sgang            ###   ########.fr       */
+/*   Updated: 2020/07/14 19:59:35 by sgang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@ int	ft_iterative_factorial(int nb)
 	int ret;
 	int idx;
 
+	if (nb < 0)
+		return (0);
 	idx = 0;
 	ret = 1;
 	while (++idx <= nb)
