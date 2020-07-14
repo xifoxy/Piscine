5cb5ff5881b2be2107438713e587c106730a98a7
Sunghyun Gang <sgang@c4r3s4.42seoul.kr>
2020-07-14 17:37:03 +0900

    C02

--- C02/ex01/ft_strncpy.c
+++ C02/ex01/ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: sgang <xifoxy.ru@gmail.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/06 17:48:37 by sgang             #+#    #+#             */
-/*   Updated: 2020/07/14 16:11:19 by sgang            ###   ########.fr       */
+/*   Updated: 2020/07/14 17:27:00 by sgang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,13 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 	unsigned int idx;
 
 	idx = 0;
-	while (idx < n && src[idx])
-		dest[idx] = src[idx++];
-	while (idx < n)
-		dest[idx++] = 0;
+	while (idx < n && src[idx]){
+		dest[idx] = src[idx];
+		++idx;
+	}
+	while (idx < n){
+		dest[idx] = 0;
+		++idx;
+	}
 	return (dest);
 }
